cc69057dfd1c35c7c4b4803fe0b56fd0d62737a8
Butenko Pavel <pavlo.butenko@nixsolutions.com>
2022-12-09 17:21:48 +0200

    Add enpoints for login

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
 import { getRegisterPath } from '@constants/api';
+import { getLoginPath } from '@constants/api';
+import { getSocialLoginPath } from '@constants/api';
 
 export const setUser = data => axios.post(getRegisterPath, data);
+export const userLogin = data => axios.post(getLoginPath, data);
+export const socialLogin = data => axios.post(getSocialLoginPath, data);
