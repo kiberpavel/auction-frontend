a8146a6cb174bfeeb9e873ac2d96578c37fbc874
Butenko Pavel <pavlo.butenko@nixsolutions.com>
2023-01-11 18:52:36 +0200

    Update env file and add endpoints

--- src/feature/CreateLotPage/CreateLotPage.jsx
+++ src/feature/CreateLotPage/CreateLotPage.jsx
@@ -1,47 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import { Alert, Button } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import lotsSelectors from '@store/lots/lots-selectors';
-import { useDispatch } from 'react-redux';
-import {
-  setShortName,
-  setDescription,
-  setPrice,
-  createLot,
-} from '@store/lots/lotsSlice';
+import LotForm from '@common/LotForm/LotForm';
+import { createLot } from '@store/lots/lotsSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import usersSelectors from '@store/users/users-selectors';
 import { getCurrentUser } from '@store/users/usersSlice';
+import lotsSelectors from '@store/lots/lots-selectors';
 
 const CreateLotPage = () => {
-  const dispatch = useDispatch();
-  const [selectedFile, setSelectedFile] = useState(null);
-  const shortName = useSelector(lotsSelectors.getShortName);
-  const description = useSelector(lotsSelectors.getDescription);
-  const price = useSelector(lotsSelectors.getPrice);
-  const userId = useSelector(usersSelectors.getId);
-  const hasError = useSelector(lotsSelectors.getLotErrorStatus);
-  const error = useSelector(lotsSelectors.getLotErrorMessage);
-  const message = useSelector(lotsSelectors.getLotMessage);
   let formData = new FormData();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getCurrentUser());
   });
 
-  const handleFileSelect = event => {
-    setSelectedFile(event.target.files[0]);
-  };
-  const setLotShortName = event => {
-    dispatch(setShortName(event.target.value));
-  };
-  const setLotDescription = event => {
-    dispatch(setDescription(event.target.value));
-  };
-  const setLotPrice = event => {
-    dispatch(setPrice(Number(event.target.value)));
-  };
-
+  const [selectedFile, setSelectedFile] = useState(null);
+  const userId = useSelector(usersSelectors.getId);
+  const shortName = useSelector(lotsSelectors.getShortName);
+  const description = useSelector(lotsSelectors.getDescription);
+  const price = useSelector(lotsSelectors.getPrice);
   const submitForm = event => {
     formData.append('lot_image', selectedFile);
     formData.append('short_name', shortName);
@@ -53,55 +30,13 @@ const CreateLotPage = () => {
   };
   return (
     <div>
-      <section className="mx-auto w-50">
-        {hasError === true ? (
-          <Alert key="danger" variant="danger">
-            {error}
-          </Alert>
-        ) : message ? (
-          <Alert key="success" variant="success">
-            {message}
-          </Alert>
-        ) : (
-          ''
-        )}
-      </section>
-      <Form className="w-50">
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Short name</Form.Label>
-          <Form.Control
-            type="text"
-            value={shortName}
-            placeholder="Vase"
-            onChange={setLotShortName}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Description</Form.Label>
-          <Form.Control
-            type="text"
-            value={description}
-            placeholder="Good condition"
-            onChange={setLotDescription}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Price</Form.Label>
-          <Form.Control
-            type="number"
-            value={price}
-            placeholder="3.4"
-            onChange={setLotPrice}
-          />
-        </Form.Group>
-        <Form.Group controlId="formFile" className="mb-3">
-          <Form.Label>File</Form.Label>
-          <Form.Control type="file" onChange={handleFileSelect} />
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={submitForm}>
-          Submit
-        </Button>
-      </Form>
+      <LotForm
+        submitForm={submitForm}
+        setSelectedFile={setSelectedFile}
+        shortName={shortName}
+        description={description}
+        price={price}
+      />
     </div>
   );
 };
