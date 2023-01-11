14931810e5289daa5d6168686b2caed4d2641413
Butenko Pavel <pavlo.butenko@nixsolutions.com>
2023-01-11 18:54:31 +0200

    Update lot store

--- src/store/lots/lotsSlice.js
+++ src/store/lots/lotsSlice.js
@@ -1,11 +1,47 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { lotCreation } from '@api/lot';
+import { lotCreation, lotList, lotDelete, lotUpdate } from '@api/lot';
 
 export const createLot = createAsyncThunk(
   'lot/createLot',
   async (data, { rejectWithValue }) => {
     try {
       const response = await lotCreation(data);
+      return response.data;
+    } catch ({ response }) {
+      return rejectWithValue(response.data.error);
+    }
+  },
+);
+
+export const listOfLots = createAsyncThunk(
+  'lot/listOfLots',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await lotList();
+      return response.data;
+    } catch ({ response }) {
+      return rejectWithValue(response.data.error);
+    }
+  },
+);
+
+export const deleteLot = createAsyncThunk(
+  'lot/deleteLot',
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await lotDelete(data);
+      return response.data;
+    } catch ({ response }) {
+      return rejectWithValue(response.data.error);
+    }
+  },
+);
+
+export const editLot = createAsyncThunk(
+  'lot/editLot',
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await lotUpdate(data);
       return response.data;
     } catch ({ response }) {
       return rejectWithValue(response.data.error);
@@ -21,6 +57,10 @@ const initialState = {
   hasError: false,
   message: '',
   errorMessage: '',
+  lots: {},
+  listOfLotsStatus: false,
+  status: '',
+  id: '',
 };
 
 export const lotsSlice = createSlice({
@@ -38,6 +78,21 @@ export const lotsSlice = createSlice({
     },
     setImage: (state, action) => {
       state.image = action.payload;
+    },
+    setStatus: (state, action) => {
+      state.status = action.payload;
+    },
+    setId: (state, action) => {
+      state.id = action.payload;
+    },
+    setHasErrors: (state, action) => {
+      state.hasError = action.payload;
+    },
+    setErrorMessage: (state, action) => {
+      state.errorMessage = action.payload;
+    },
+    setMessage: (state, action) => {
+      state.message = action.payload;
     },
   },
   extraReducers: builder => {
@@ -57,10 +112,30 @@ export const lotsSlice = createSlice({
       state.hasError = true;
       state.errorMessage = action.payload;
     });
+    builder.addCase(listOfLots.fulfilled, (state, action) => {
+      state.lots = action.payload;
+      state.listOfLotsStatus = true;
+    });
+    builder.addCase(editLot.fulfilled, (state, action) => {
+      state.message = action.payload.message;
+    });
+    builder.addCase(editLot.rejected, (state, action) => {
+      state.hasError = true;
+      state.errorMessage = action.payload;
+    });
   },
 });
 
-export const { setPrice, setShortName, setDescription, setImage } =
-  lotsSlice.actions;
+export const {
+  setPrice,
+  setShortName,
+  setDescription,
+  setImage,
+  setStatus,
+  setId,
+  setHasErrors,
+  setErrorMessage,
+  setMessage,
+} = lotsSlice.actions;
 
 export default lotsSlice.reducer;
