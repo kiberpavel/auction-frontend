c47a96e110796641d0dd09ba5ee7ed9b6640aea4
Butenko Pavel <pavlo.butenko@nixsolutions.com>
2023-01-11 19:07:04 +0200

    Update routes list and add navlink to header

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,6 +3,8 @@ import HomePage from '@feature/HomePage/HomePage';
 import LoginPage from '@feature/LoginPage/LoginPage';
 import ProfilePage from '@feature/ProfilePage/ProfilePage';
 import CreateLotPage from '@feature/CreateLotPage/CreateLotPage';
+import LotListPage from '@feature/LotListPage/LotListPage';
+import EditLotPage from '@feature/EditLotPage/EditLotPage';
 
 const routes = [
   {
@@ -33,6 +35,18 @@ const routes = [
     id: 5,
     path: '/lot/create',
     element: <CreateLotPage />,
+    protected: true,
+  },
+  {
+    id: 6,
+    path: '/lot/list',
+    element: <LotListPage />,
+    protected: true,
+  },
+  {
+    id: 7,
+    path: '/lot/update',
+    element: <EditLotPage />,
     protected: true,
   },
 ];
