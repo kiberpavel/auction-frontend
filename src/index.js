a9e1c46905c6f2e39737d8bb7422372df5945d91
Butenko Pavel <pavlo.butenko@nixsolutions.com>
2023-01-18 18:25:32 +0200

    Change location of header and footer

--- src/index.js
+++ src/index.js
@@ -7,13 +7,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '@store/store';
 import Router from '@router/Router';
+import Header from '@common/Header';
+import Footer from '@common/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <Header />
         <Router />
+        <Footer />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
