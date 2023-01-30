e98c34fa93e2b88a512db495795b55035b72d331
Butenko Pavel <pavlo.butenko@nixsolutions.com>
2023-01-30 16:56:19 +0200

    Add endpoint for up lot price

--- src/api/lot.js
+++ src/api/lot.js
@@ -4,6 +4,7 @@ import {
   getLotDeletePath,
   getLotUpdatePath,
   getLotListPath,
+  getLotPriceUpPath,
 } from '@constants/api';
 import axios from 'axios';
 
@@ -12,3 +13,4 @@ export const lotDelete = data => authAxios.post(getLotDeletePath, data);
 export const lotUpdate = data => authAxios.post(getLotUpdatePath, data);
 export const lotList = authStatus =>
   !authStatus ? axios.get(getLotListPath) : authAxios.get(getLotListPath);
+export const lotPriceUp = data => authAxios.post(getLotPriceUpPath, data);
