6250d00935aaaa6193273af56753740f45ef7da0
Butenko Pavel <pavlo.butenko@nixsolutions.com>
2023-02-06 11:00:20 +0200

    Update orders store

--- src/store/orders/orderSlice.js
+++ src/store/orders/orderSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { orderSet } from '@api/order';
+import { orderSet, orderListForUser, orderPay } from '@api/order';
 
 export const addOrder = createAsyncThunk(
   'order/addOrder',
@@ -13,11 +13,39 @@ export const addOrder = createAsyncThunk(
   },
 );
 
+export const userOrders = createAsyncThunk(
+  'order/userOrders',
+  async (userId, { rejectWithValue }) => {
+    try {
+      const response = await orderListForUser(userId);
+      return response.data;
+    } catch ({ response }) {
+      return rejectWithValue(response.data.error);
+    }
+  },
+);
+
+export const payOrders = createAsyncThunk(
+  'order/payOrders',
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await orderPay(data);
+      return response.data;
+    } catch ({ response }) {
+      return rejectWithValue(response.data.error);
+    }
+  },
+);
+
 const initialState = {
   message: '',
   hasError: false,
   errorMessage: '',
   time: null,
+  count: 0,
+  userOrders: {},
+  totalSum: 0,
+  ordersId: {},
 };
 
 export const orderSlice = createSlice({
@@ -36,6 +64,18 @@ export const orderSlice = createSlice({
     setTime: (state, action) => {
       state.time = action.payload;
     },
+    setCount: (state, action) => {
+      state.count = action.payload;
+    },
+    setUserOrders: (state, action) => {
+      state.userOrders = action.payload;
+    },
+    setTotalSum: (state, action) => {
+      state.totalSum = action.payload;
+    },
+    setOrdersId: (state, action) => {
+      state.ordersId = action.payload;
+    },
   },
   extraReducers: builder => {
     builder.addCase(addOrder.fulfilled, (state, action) => {
@@ -44,9 +84,24 @@ export const orderSlice = createSlice({
     builder.addCase(addOrder.rejected, (state, action) => {
       state.errorMessage = action.payload;
     });
+    builder.addCase(userOrders.fulfilled, (state, action) => {
+      state.count = action.payload.count;
+      state.userOrders = action.payload.data;
+    });
+    builder.addCase(payOrders.fulfilled, (state, action) => {
+      state.message = action.payload.message;
+    });
   },
 });
 
-export const { setMessage, setHasError, setErrorMessage, setTime } =
-  orderSlice.actions;
+export const {
+  setMessage,
+  setHasError,
+  setErrorMessage,
+  setTime,
+  setCount,
+  setUserOrders,
+  setTotalSum,
+  setOrdersId,
+} = orderSlice.actions;
 export default orderSlice.reducer;
